104c188cad9a48a138ed95896f2421adbead40c4
e-mags <e.magsipoc@mail.utoronto.ca>
2021-07-22 16:19:19 -0400

    Update test script Currently only compares the serial and concurrent processing of HDF file Should compare against regular pv.read function

--- openfdem/displacement.py
+++ openfdem/displacement.py
@@ -2,6 +2,7 @@ import glob
 import os
 import os.path as path
 import re
+from itertools import repeat
 
 import pyvista as pv
 import time
@@ -18,25 +19,30 @@ def calc_timer_values(end_time):
     else:
         return ("\033[1m%d minutes and %d seconds\033[0m." % (minutes, sec))
 
-start = time.time()
-model = fd.Model("../example_outputs/Irazu_UCS")
-#model = fd.Model("../Irazu_UCS")
-f_names = (model._basic_files)
-
-history_strain = []  # List to hold the stress_history
-avg_platen_disp = [0.0, 0.0, 0.0]  # Dummy cell
-axis_of_loading = 1  # Axis of loading in Y direction.
-
-## Get rock dimension.
-model.rock_sample_dimensions()
-## Check UCS Simulation
-if model.simulation_type() != "UCS Simulation":
-    print("Simulation appears to be not for compressive strength")
-
 # Load each timestep
 # backup copy compressed below
 
-def history_strain_func(f_name):
+def history_strain_func_hdf(f_name,model):
+    openfdem_model_ts = model.storage.read_file(f_name)
+    platen = (openfdem_model_ts.threshold([model.platen_cells_elem_id, model.platen_cells_elem_id],
+                                          model.var_data["mineral_type"]))
+    top, bottom = (platen.get_data_range(model.var_data["boundary"]))
+
+    avg_top_platen_disp = model.platen_info(openfdem_model_ts, top, model.var_data["platen_displacement"])
+    avg_bottom_platen_disp = model.platen_info(openfdem_model_ts, bottom,
+                                              model.var_data["platen_displacement"])
+    
+    avg_platen_disp = [0.0, 0.0, 0.0]  # Dummy cell
+    axis_of_loading = 1  # Axis of loading in Y direction.
+
+    for i in range(0, model.number_of_points_per_cell):
+        avg_platen_disp[i] = abs(avg_top_platen_disp[i]) + abs(avg_bottom_platen_disp[i])
+
+    strain_from_platen = avg_platen_disp[axis_of_loading] / model.sample_height * 100.0
+
+    return strain_from_platen
+
+def history_strain_func(f_name,model):
     openfdem_model_ts = pv.read(f_name)
     platen = (openfdem_model_ts.threshold([model.platen_cells_elem_id, model.platen_cells_elem_id],
                                           model.var_data["mineral_type"]))
@@ -45,23 +51,46 @@ def history_strain_func(f_name):
     avg_top_platen_disp = model.platen_info(openfdem_model_ts, top, model.var_data["platen_displacement"])
     avg_bottom_platen_disp = model.platen_info(openfdem_model_ts, bottom,
                                               model.var_data["platen_displacement"])
+    
+    avg_platen_disp = [0.0, 0.0, 0.0]  # Dummy cell
+    axis_of_loading = 1  # Axis of loading in Y direction.
 
     for i in range(0, model.number_of_points_per_cell):
         avg_platen_disp[i] = abs(avg_top_platen_disp[i]) + abs(avg_bottom_platen_disp[i])
 
     strain_from_platen = avg_platen_disp[axis_of_loading] / model.sample_height * 100.0
-    history_strain.append(strain_from_platen)
 
     return strain_from_platen
 
-def main():
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        results = executor.map(history_strain_func, f_names)  # is self the list we are iterating over
-    return results
-
 if __name__ == '__main__':
-    main()
+    
+    model = fd.Model(r"X:\20210622_irazu_example\results")
+    #model = fd.Model("../Irazu_UCS")
+    f_names = (model._basic_files)
+
+    ## Get rock dimension.
+    model.rock_sample_dimensions()
+    ## Check UCS Simulation
+    if model.simulation_type() != "UCS Simulation":
+        print("Simulation appears to be not for compressive strength")
+    
+    print("Start for loop")
+    start = time.time()
+    result = []
+    for fname in f_names:
+        result.append(history_strain_func_hdf(fname,model))
+    print(len(result))
+    print(calc_timer_values(time.time()-start))
+
+
+    print(f"Start concurrent")
+    start = time.time()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+        results = list(executor.map(history_strain_func_hdf, f_names,repeat(model),chunksize=len(f_names)//os.cpu_count()))  # is self the list we are iterating over
+    print(len(results))
     print(calc_timer_values(time.time() - start))
+    
+    
 
 
 # platen_displacement(model)
