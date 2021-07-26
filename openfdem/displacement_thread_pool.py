745fe755c2747711d31460000aa147599a2d760e
EkaterinaOssetchkina <ekaterina.ossetchkina@mail.utoronto.ca>
2021-07-26 10:00:20 -0400

    added displacement_thread_pool.py which uses the threadpoolexecutor also, added a break statement in displacement.py to make the executor map function work

--- openfdem/displacement_thread_pool.py
+++ openfdem/displacement_thread_pool.py
@@ -1,5 +1,4 @@
 import glob
-import multiprocessing
 import os
 import os.path as path
 import re
@@ -9,8 +8,10 @@ import pyvista as pv
 import time
 import concurrent.futures
 from multiprocessing import Process
-from threading import Thread
+import threading
 import openfdem as fd
+
+
 ## Model attributes
 
 def calc_timer_values(end_time):
@@ -20,10 +21,11 @@ def calc_timer_values(end_time):
     else:
         return ("\033[1m%d minutes and %d seconds\033[0m." % (minutes, sec))
 
+
 # Load each timestep
 # backup copy compressed below
 
-def history_strain_func_hdf(f_name,model):
+def history_strain_func_hdf(f_name, model):
     openfdem_model_ts = model.storage.read_file(f_name)
     platen = (openfdem_model_ts.threshold([model.platen_cells_elem_id, model.platen_cells_elem_id],
                                           model.var_data["mineral_type"]))
@@ -31,8 +33,8 @@ def history_strain_func_hdf(f_name,model):
 
     avg_top_platen_disp = model.platen_info(openfdem_model_ts, top, model.var_data["platen_displacement"])
     avg_bottom_platen_disp = model.platen_info(openfdem_model_ts, bottom,
-                                              model.var_data["platen_displacement"])
-    
+                                               model.var_data["platen_displacement"])
+
     avg_platen_disp = [0.0, 0.0, 0.0]  # Dummy cell
     axis_of_loading = 1  # Axis of loading in Y direction.
 
@@ -43,7 +45,8 @@ def history_strain_func_hdf(f_name,model):
 
     return strain_from_platen
 
-def history_strain_func(f_name,model):
+
+def history_strain_func(f_name, model):
     openfdem_model_ts = pv.read(f_name)
     platen = (openfdem_model_ts.threshold([model.platen_cells_elem_id, model.platen_cells_elem_id],
                                           model.var_data["mineral_type"]))
@@ -51,8 +54,8 @@ def history_strain_func(f_name,model):
 
     avg_top_platen_disp = model.platen_info(openfdem_model_ts, top, model.var_data["platen_displacement"])
     avg_bottom_platen_disp = model.platen_info(openfdem_model_ts, bottom,
-                                              model.var_data["platen_displacement"])
-    
+                                               model.var_data["platen_displacement"])
+
     avg_platen_disp = [0.0, 0.0, 0.0]  # Dummy cell
     axis_of_loading = 1  # Axis of loading in Y direction.
 
@@ -63,8 +66,9 @@ def history_strain_func(f_name,model):
 
     return strain_from_platen
 
+
 if __name__ == '__main__':
-    
+
     model = fd.Model("../example_outputs/Irazu_UCS")
     f_names = (model._basic_files)
 
@@ -78,76 +82,78 @@ if __name__ == '__main__':
     start = time.time()
     result = []
     for fname in f_names:
-        result.append(history_strain_func(fname,model))
+        result.append(history_strain_func(fname, model))
     print(len(result))
-    print(calc_timer_values(time.time()-start))
-
+    print(calc_timer_values(time.time() - start))
 
     print(f"Start concurrent")
     start = time.time()
-    with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
-        results = list(executor.map(history_strain_func, f_names,repeat(model),chunksize=len(f_names)//os.cpu_count()))  # is self the list we are iterating over
+    max=100
+    with concurrent.futures.ThreadPoolExecutor(max) as executor:
+        results = list(executor.map(history_strain_func, f_names, repeat(model),
+                                    chunksize=len(f_names) //max))  # is self the list we are iterating over
+        print(f'the current thread count is: %s' % threading.activeCount())
     print(len(results))
     print(calc_timer_values(time.time() - start))
 
     print(f"Start concurrent optimized n")
-    for i in range(1,10):
-        n = 2**i
-        print(f"n = {n}, chunksize = {len(f_names)//n}")
+    for i in range(1, 10):
+        n = 2 ** i
+        print(f"n = {n}, chunksize = {len(f_names) // n}")
         start = time.time()
-        if (len(f_names)//n) < 1:
-            break
-        else:
-            with concurrent.futures.ProcessPoolExecutor() as executor:
-                results = list(executor.map(history_strain_func, f_names,repeat(model),chunksize=len(f_names)//n))  # is self the list we are iterating over
-            print(len(results))
-            print(calc_timer_values(time.time() - start))
-
+        with concurrent.futures.ThreadPoolExecutor(n) as executor:
+            results = list(executor.map(history_strain_func, f_names, repeat(model),
+                                        chunksize=len(f_names) // n))  # is self the list we are iterating over
+            print(f'the current thread count is: %s' % threading.activeCount())
+        print(len(results))
+        print(calc_timer_values(time.time() - start))
 
     print("chunksize = 1")
     start = time.time()
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        results = list(executor.map(history_strain_func, f_names,repeat(model)))  # is self the list we are iterating over
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(
+            executor.map(history_strain_func, f_names, repeat(model)))  # is self the list we are iterating over
     print(len(results))
     print(calc_timer_values(time.time() - start))
 
-
+    ##--------------------------------------------------------------
     print("---- HDF tests ----")
     print("Start for loop")
     start = time.time()
     result = []
     for fname in f_names:
-        result.append(history_strain_func_hdf(fname,model))
+        result.append(history_strain_func_hdf(fname, model))
     print(len(result))
-    print(calc_timer_values(time.time()-start))
-
+    print(calc_timer_values(time.time() - start))
 
     print(f"Start concurrent")
     start = time.time()
-    with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
-        results = list(executor.map(history_strain_func_hdf, f_names,repeat(model),chunksize=len(f_names)//os.cpu_count()))  # is self the list we are iterating over
+    with concurrent.futures.ThreadPoolExecutor(max) as executor:
+        results = list(executor.map(history_strain_func_hdf, f_names, repeat(model),
+                                    chunksize=len(f_names) // max))  # is self the list we are iterating over
+        print(f'the current thread count is: %s' % threading.activeCount())
     print(len(results))
     print(calc_timer_values(time.time() - start))
 
     print(f"Start concurrent optimized n")
-    for i in range(1,10):
-        n = 2**i
-        print(f"n = {n}, chunksize = {len(f_names)//n}")
+    for i in range(1, 10):
+        n = 2 ** i
+        print(f"n = {n}, chunksize = {len(f_names) // n}")
         start = time.time()
-        if (len(f_names)//n)<1:
-            break
-        else:
-            with concurrent.futures.ProcessPoolExecutor() as executor:
-                results = list(executor.map(history_strain_func_hdf, f_names,repeat(model),chunksize=len(f_names)//n))  # is self the list we are iterating over
-            print(len(results))
-            print(calc_timer_values(time.time() - start))
-    
+        with concurrent.futures.ThreadPoolExecutor(n) as executor:
+            results = list(executor.map(history_strain_func_hdf, f_names, repeat(model),
+                                        chunksize=len(f_names) // n))  # is self the list we are iterating over
+            print(f'the current threads are = %s' % threading.activeCount())
+        print(len(results))
+        print(len(f_names) // n)
+        print(calc_timer_values(time.time() - start))
+
     print("chunksize = 1")
     start = time.time()
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        results = list(executor.map(history_strain_func_hdf, f_names,repeat(model)))  # is self the list we are iterating over
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(
+            executor.map(history_strain_func_hdf, f_names, repeat(model)))  # is self the list we are iterating over
     print(len(results))
     print(calc_timer_values(time.time() - start))
-
 
 # platen_displacement(model)
